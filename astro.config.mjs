e493b32502c031e0a146d05b93b80848fc2fad1a
Corvus <a.mejiabjs@protonmail.com>
2025-07-24 12:05:38 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-export default defineConfig(
-  {
-  output: "static",
-  base: "/nombre-del-repo/", // ⚠️ si tu repo se llama 'mi-sitio', usa '/mi-sitio/'
+import netlify from "@astrojs/netlify";
+
+export default defineConfig({
+  output: "server", // importante
+  adapter: netlify(),
 });
 
 
